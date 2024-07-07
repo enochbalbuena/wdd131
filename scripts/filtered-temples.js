e70a47c496b4d74232893124c4d4dcb09fb9d1bc
enochbalbuena <enxch@icloud.com>
2024-07-06 23:53:47 -0600

    improved image size

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -17,70 +17,70 @@ document.addEventListener("DOMContentLoaded", function() {
             location: "Aba, Nigeria",
             dedicated: "2005, August, 7",
             area: 11500,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+            imageUrl: "./images/aba-nigeria.webp"
         },
         {
             templeName: "Manti Utah",
             location: "Manti, Utah, United States",
             dedicated: "1888, May, 21",
             area: 74792,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+            imageUrl: "./images/manti-utah.webp"
         },
         {
             templeName: "Payson Utah",
             location: "Payson, Utah, United States",
             dedicated: "2015, June, 7",
             area: 96630,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+            imageUrl: "./images/payson-utah.webp"
         },
         {
             templeName: "Yigo Guam",
             location: "Yigo, Guam",
             dedicated: "2020, May, 2",
             area: 6861,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+            imageUrl: "./images/yigo-guam.webp"
         },
         {
             templeName: "Washington D.C.",
             location: "Kensington, Maryland, United States",
             dedicated: "1974, November, 19",
             area: 156558,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+            imageUrl: "./images/washington-dc.webp"
         },
         {
             templeName: "Lima Perú",
             location: "Lima, Perú",
             dedicated: "1986, January, 10",
             area: 9600,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+            imageUrl: "./images/lima-peru.webp"
         },
         {
             templeName: "Mexico City Mexico",
             location: "Mexico City, Mexico",
             dedicated: "1983, December, 2",
             area: 116642,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+            imageUrl: "./images/mexico-city-mexico.webp"
         },
         {
             templeName: "Guayaquil Ecuador",
             location: "Guayaquil, Ecuador",
             dedicated: "1999, August, 1",
             area: 73925,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/guayaquil-ecuador/400x250/guayaquil-ecuador-temple-lds-884500-wallpaper.jpg"
+            imageUrl: "./images/guayaquil-ecuador.webp"
         },
         {
             templeName: "Hamilton New Zealand",
             location: "Hamilton, New Zealand",
             dedicated: "1958, April, 20",
             area: 44232,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/hamilton-new-zealand/400x250/hamilton-new-zealand-lds-temple-942155-wallpaper.jpg"
+            imageUrl: "./images/hamilton-new-zealand.webp"
         },
         {
             templeName: "Houston Texas",
             location: "Houston, Texas, United States",
             dedicated: "2000, August, 26",
             area: 33145,
-            imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/houston-texas/2018/400x250/houston-temple04.jpg"
+            imageUrl: "./images/houston-texas.webp"
         }
     ];
 
@@ -127,5 +127,6 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+    // Display all temples initially
     displayTemples(temples);
 });
